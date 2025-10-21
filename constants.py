e113ae9b3ed2773a6ab4a9678ce85fba514983ba
rafik_sameh <rafiksameh@outlook.com>
2025-10-21 23:43:39 +0300

    update consts

--- constants.py
+++ constants.py
@@ -2,5 +2,5 @@ PDF_PAGES_FOLDER = "tmp/pdf_pages"
 PDF_IMAGE_FOLDER = "tmp/pdf_images"
 UPLOADS_FOLDER = "tmp/uploads"
 URI_PATH = "tmp/lancedb"
-PDF_PATH = "F:/Data science training/LLM/agno/UserManual.pdf"
+PDF_PATH = "F:/Data science training/LLM/agno/UPM_User_Guide_070121.pdf"
 
